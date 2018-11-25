e833fa2b935507443fa24c2707c0b3daf39aee22
madhukarreddygangapuram <gangamadhu1234@gmail.com>
2018-11-25 13:24:05 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
+  state={
+    name:"suresh",
+    age:25
+  }
+  dataChange1=(newName,newAge)=>{
+    this.setState({
+      name:newName,
+      age:newAge
+     
+    })
+  }
+  
+  binding=(event)=>{
+    this.setState({
+      name:event.target.value
+    
+    })
+  }
+  binding1=(event)=>{
+    this.setState({
+      
+      age:event.target.value
+    })
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <br/>
+       <button onClick={()=>this.dataChange1('hello india',45)}>DataChangeButton</button>
+       <h1>{this.state.name}</h1>
+       <button onClick={this.dataChange1.bind(this,'hello worlds',44)}>Enter Some thing</button>
+       <br/>
+      
+       <input type="text"  onChange={this.binding} value={this.state.name}/>
+       <input type="text"  onChange={this.binding1} value={this.state.age}/>
+
       </div>
     );
   }
---
+++ src/components/parentToChid/child.js
@@ -0,0 +1,13 @@
+import React  from 'react';
+
+const child=()=>{
+    
+        return (
+            <div>
+                <button></button>
+            </div>
+        );
+        }
+
+
+export default child;
